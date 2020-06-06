cb90a8cd7c5fdef9d12a2a7aa788737511c369dd
pizssn <48932417+pizssn@users.noreply.github.com>
2020-06-06 23:10:02 +0800

    Update QuickNav.vue

--- sl-vue/src/components/home/QuickNav.vue
+++ sl-vue/src/components/home/QuickNav.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="quick-nav">
       <img src="../../assets/img/icon/icon1.png" alt="" class="monster">
-      <a href="https://github.com/Antabot/White-Jotter/blob/master/README.md" target="_blank">
+      <a href="https://https://github.com/pizssn/Space-Library" target="_blank">
         <img src="../../assets/img/icon/icon5.png" alt="" class="readme">
       </a>
       <img src="../../assets/img/icon/icon7.png" alt="" class="spark">
